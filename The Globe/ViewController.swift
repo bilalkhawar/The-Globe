dd6aefbbe1b5f4993547a201db8b31bb13d4c286
Bilal Khawar <bilal.khawar@outlook.com>
2024-10-15 00:32:11 -0400

    Initial Commit

---
+++ The Globe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Globe
+//
+//  Created by Bilal Khawar on 2024-10-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
