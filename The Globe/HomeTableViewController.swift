410738c420a5f7d7b4fa43e7f21a8c6f0c9c95f2
Bilal Khawar <bilal.khawar@outlook.com>
2024-10-15 11:42:06 -0400

    fetching articles in HomeTableViewController and updating the data source

--- The Globe/HomeTableViewController.swift
+++ The Globe/HomeTableViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class HomeTableViewController: UITableViewController {
     
+    private var requestResponse = RequestResponse()
+    private var articles = [Article]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchArticles()
     }
     
+    // MARK: - Private Methods
+    
+    private func fetchArticles() {
+        Task {
+            do {
+                let recommendations = try await requestResponse.fetchRecommendedArticles()
+                articles = recommendations.articles
+            } catch let error as ArticleFetchError {
+                let alert = UIAlertController(title: error.title(), message: error.message(), preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString(CustomText.Alert.okAction, comment: "Default action"), style: .default, handler: { _ in
+                }))
+                self.present(alert, animated: true, completion: nil)
+
+            }
+        }
+    }
 }
